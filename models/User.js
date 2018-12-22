e159f9f4a1a87f3d6c2c601c3890d8da958958da
afw821 <afw821@gmail.com>
2018-12-22 16:51:10 -0500

    change to input field

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 
-// const bcrypt = require('bcryptjs');
-// SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
+SALT_WORK_FACTOR = 10;
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -19,34 +19,34 @@ const UserSchema = new Schema ({
   
 });
 
-// //BCRYPT JS to encrypt users password on the database
-// UserSchema.pre('save', function(next) {
-//     const user = this;
+//BCRYPT JS to encrypt users password on the database
+UserSchema.pre('save', function(next) {
+    const user = this;
 
-//     // only hash the password if it has been modified (or is new)
-//     if (!user.isModified('password')) return next();
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
 
-//     // generate a salt
-//     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//         if (err) return next(err);
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
 
-//         // hash the password using our new salt
-//         bcrypt.hash(user.password, salt, function(err, hash) {
-//             if (err) return next(err);
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
 
-//             // override the cleartext password with the hashed one
-//             user.password = hash;
-//             next();
-//         });
-//     });
-// });
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
 
-// UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-//         if (err) return cb(err);
-//         cb(null, isMatch);
-//     });
-// };
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 const User = mongoose.model("User", UserSchema);
 
